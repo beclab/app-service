cda00775255533defeef3ab5116470dfab950848
hysyeah <hysyeah@gmail.com>
2024-05-08 20:46:45 +0800

    fixed: kubelet probe failed with envoy redirect (#2)

--- pkg/sandbox/sidecar/set_cookie_tpl.go
+++ pkg/sandbox/sidecar/set_cookie_tpl.go
@@ -3,6 +3,8 @@ package sidecar
 import (
 	"bytes"
 	"text/template"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 const envoySetCookie = `
@@ -129,4 +131,19 @@ func genEnvoySetCookieScript(appDomains []string) ([]byte, error) {
 		return []byte{}, err
 	}
 	return envoySetCookie.Bytes(), nil
+}
+
+func getHTTProbePath(pod *corev1.Pod) (probesPath []string) {
+	for _, c := range pod.Spec.Containers {
+		if c.LivenessProbe != nil && c.LivenessProbe.HTTPGet != nil {
+			probesPath = append(probesPath, c.LivenessProbe.HTTPGet.Path)
+		}
+		if c.ReadinessProbe != nil && c.ReadinessProbe.HTTPGet != nil {
+			probesPath = append(probesPath, c.ReadinessProbe.HTTPGet.Path)
+		}
+		if c.StartupProbe != nil && c.StartupProbe.HTTPGet != nil {
+			probesPath = append(probesPath, c.StartupProbe.HTTPGet.Path)
+		}
+	}
+	return probesPath
 }
