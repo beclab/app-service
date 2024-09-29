7eb7f3b02910e1c965d6371498cbcb319115cfb8
hysyeah <hysyeah@gmail.com>
2024-09-29 19:35:57 +0800

    fix: add downloading app to list

--- pkg/apiserver/handler_service.go
+++ pkg/apiserver/handler_service.go
@@ -134,6 +134,51 @@ func (h *Handler) listBackend(req *restful.Request, resp *restful.Response) {
 
 	owner := req.PathParameter(ParamUserName)
 
+	appsMap := make(map[string]appv1alpha1.Application)
+
+	// get downloading, installing state app from app managers
+	ams, err := appClient.AppV1alpha1().ApplicationManagers().List(req.Request.Context(), metav1.ListOptions{})
+	if err != nil {
+		api.HandleError(resp, req, err)
+		return
+	}
+	for _, am := range ams.Items {
+		if am.Spec.Type != appv1alpha1.App {
+			continue
+		}
+		if am.Spec.AppOwner == owner && (am.Status.State == appv1alpha1.Downloading || am.Status.State == appv1alpha1.Installing) {
+			var appConfig appinstaller.ApplicationConfig
+			err = json.Unmarshal([]byte(am.Spec.Config), &appConfig)
+			if err != nil {
+				api.HandleError(resp, req, err)
+				return
+			}
+			now := metav1.Now()
+			name, _ := utils.FmtAppMgrName(am.Spec.AppName, owner, appConfig.Namespace)
+			app := appv1alpha1.Application{
+				TypeMeta: metav1.TypeMeta{},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:              name,
+					CreationTimestamp: am.CreationTimestamp,
+				},
+				Spec: appv1alpha1.ApplicationSpec{
+					Name:      am.Spec.AppName,
+					Appid:     utils.GetAppID(am.Spec.AppName),
+					Namespace: am.Spec.AppNamespace,
+					Owner:     owner,
+					Entrances: appConfig.Entrances,
+					Icon:      appConfig.Icon,
+				},
+				Status: appv1alpha1.ApplicationStatus{
+					State:      appv1alpha1.Installing.String(),
+					StatusTime: &now,
+					UpdateTime: &now,
+				},
+			}
+			appsMap[app.Name] = app
+		}
+	}
+
 	// run with request context for incoming client
 	allApps, err := appClient.AppV1alpha1().Applications().List(req.Request.Context(), metav1.ListOptions{})
 	if err != nil {
@@ -145,8 +190,12 @@ func (h *Handler) listBackend(req *restful.Request, resp *restful.Response) {
 	var filteredApps []appv1alpha1.Application
 	for _, a := range allApps.Items {
 		if a.Spec.Owner == owner {
-			filteredApps = append(filteredApps, a)
+			appsMap[a.Name] = a
 		}
+	}
+
+	for _, app := range appsMap {
+		filteredApps = append(filteredApps, app)
 	}
 
 	// sort by create time desc
