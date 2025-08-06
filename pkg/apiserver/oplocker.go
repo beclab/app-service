bdb014e8829defbce89d35f41e92a2762a0d68fa
hysyeah <hysyeah@gmail.com>
2025-08-06 16:49:26 +0800

    t

---
+++ pkg/apiserver/oplocker.go
@@ -0,0 +1,65 @@
+package apiserver
+
+import (
+	"sync"
+)
+
+type lock struct {
+	mu      sync.Mutex
+	counter uint
+}
+
+type OpLockManager struct {
+	mu       sync.Mutex
+	locksKey map[string]*lock
+}
+
+func (olm *OpLockManager) Lock(name string) {
+	olm.mu.Lock()
+	l, ok := olm.locksKey[name]
+	if !ok {
+		l = &lock{}
+		olm.locksKey[name] = l
+	}
+	l.counter++
+	olm.mu.Unlock()
+	l.mu.Lock()
+}
+
+func (olm *OpLockManager) TryLock(name string) bool {
+	olm.mu.Lock()
+	defer olm.mu.Unlock()
+
+	l, ok := olm.locksKey[name]
+	if !ok {
+		l = &lock{counter: 1}
+		olm.locksKey[name] = l
+		return true
+	}
+	if l.mu.TryLock() {
+		l.counter++
+		return true
+	}
+	return false
+}
+
+func (olm *OpLockManager) Unlock(name string) {
+	olm.mu.Lock()
+
+	l, ok := olm.locksKey[name]
+	if !ok {
+		return
+	}
+	l.counter--
+	if l.counter == 0 {
+		delete(olm.locksKey, name)
+	}
+	olm.mu.Unlock()
+	l.mu.Unlock()
+}
+
+func NewLocker() *OpLockManager {
+	return &OpLockManager{
+		locksKey: make(map[string]*lock),
+	}
+}
