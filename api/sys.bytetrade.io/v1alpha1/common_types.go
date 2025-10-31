1d5954f42dbfffce1f92dcb7d96425c417cf0878
dkeven <82354774+dkeven@users.noreply.github.com>
2025-10-31 16:28:50 +0800

    refactor: rollback env schema of desc and title (#355)

--- api/sys.bytetrade.io/v1alpha1/common_types.go
+++ api/sys.bytetrade.io/v1alpha1/common_types.go
@@ -3,14 +3,14 @@ package v1alpha1
 // EnvVarSpec defines the common fields for environment variables
 // This struct is embedded in SystemEnv, UserEnv, and AppEnvVar
 type EnvVarSpec struct {
-	EnvName     string            `json:"envName" yaml:"envName" validate:"required"`
-	Value       string            `json:"value,omitempty" yaml:"value,omitempty"`
-	Default     string            `json:"default,omitempty" yaml:"default,omitempty"`
-	Editable    bool              `json:"editable,omitempty" yaml:"editable,omitempty"`
-	Type        string            `json:"type,omitempty" yaml:"type,omitempty"`
-	Required    bool              `json:"required,omitempty" yaml:"required,omitempty"`
-	Title       map[string]string `json:"title,omitempty" yaml:"title,omitempty"`
-	Description map[string]string `json:"description,omitempty" yaml:"description,omitempty"`
+	EnvName     string `json:"envName" yaml:"envName" validate:"required"`
+	Value       string `json:"value,omitempty" yaml:"value,omitempty"`
+	Default     string `json:"default,omitempty" yaml:"default,omitempty"`
+	Editable    bool   `json:"editable,omitempty" yaml:"editable,omitempty"`
+	Type        string `json:"type,omitempty" yaml:"type,omitempty"`
+	Required    bool   `json:"required,omitempty" yaml:"required,omitempty"`
+	Title       string `json:"title,omitempty" yaml:"title,omitempty"`
+	Description string `json:"description,omitempty" yaml:"description,omitempty"`
 }
 
 // GetEffectiveValue returns the effective value of the environment variable.
